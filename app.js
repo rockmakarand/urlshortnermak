fcd63a6c0dd368aa76b744c14a9afedce501df53
rockmakarand <makarandrao0@gmail.com>
2022-11-07 20:01:59 +0530

    add dotenv

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const shortId = require('shortid')
 const createHttpError = require('http-errors')
-const mongoose = require('mongoose')
+require('dotenv').config()
 
 const path = require('path')
 const ShortUrl = require('./models/url.model');
